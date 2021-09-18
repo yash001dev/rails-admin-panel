1a82b8abb69ee64111a84d4ad6fac1d949c4e68c
yash_sanghavi_linux <y.sanghavi001@gmail.com>
2021-09-18 14:51:12 +0530

    Adding css to Admin

--- app/admin/notes.rb
+++ app/admin/notes.rb
@@ -1,5 +1,23 @@
 ActiveAdmin.register Note do
   permit_params :title, :body, :subject_id
+
+  form title: "Notes App" do |f|
+    f.inputs "Note" do
+      f.input :subject
+      f.input :title
+      f.input :body, as: :quill_editor
+    end
+    f.actions
+  end
+
+  show title: "Your Notes" do
+    h1 link_to note.title, admin_note_path
+    h4 link_to note.subject.name, admin_subject_path(note.subject)
+    div (class:'note-body') do
+      raw note.body
+    end
+  end
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
