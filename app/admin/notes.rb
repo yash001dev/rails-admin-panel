a28f0954075dde2501d0e206fd563c7b23c79ac5
yash_sanghavi_linux <y.sanghavi001@gmail.com>
2021-09-18 13:07:39 +0530

    Basic Init of Admin panel

---
+++ app/admin/notes.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Note do
+  permit_params :title, :body, :subject_id
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :title, :body, :subject_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:title, :body, :subject_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+
+end
